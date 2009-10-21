d525b362b37f1d7643a4bcdd1137b104f15a727b
Tim Sell <trsell@gmail.com>
2009-10-21 12:12:04 +0100

    useful errors + mobileSessions

--- auth.py
+++ auth.py
@@ -1,12 +1,17 @@
 import urllib, urllib2
+from urllib2 import HTTPError
 import hashlib
 from BeautifulSoup import BeautifulStoneSoup
 import os
 
 apiKey = '8fdab086c81a5bd9df3c1c4c46796ae0'
 apiSecret = '195924675c89d2da66d230292080753a'
-
 webservices = 'http://ws.audioscrobbler.com/2.0/'
+
+def md5(word):
+    md5 = hashlib.md5()
+    md5.update(word)
+    return md5.hexdigest()
 
 def getUrl(baseurl, params):
     if params:
@@ -22,20 +27,17 @@ def wsRequest(params):
 
     request = urllib2.Request(url)
     print "Sending request to " + url
-     
-    response = urllib2.urlopen(request)
+    
+    try:
+        response = urllib2.urlopen(request)
+    except HTTPError, e:
+        soup = BeautifulStoneSoup(e)
+        raise Exception('Webservice call not ok. Got:\n' +soup.prettify())
+
     soup = BeautifulStoneSoup(response)
     print soup.prettify()
-    status = soup.lfm.get('status')
-    if status != u'ok':
-        raise Exception('Webservice call not ok. Got:\n' +soup.prettify)
+    #status = soup.lfm.get('status')
     return soup.lfm
-
-def md5(word):
-    md5 = hashlib.md5()
-    md5.update(word)
-    return md5.hexdigest()
-
 
 def sign(params):
     items = params.items()
@@ -52,9 +54,7 @@ def getToken():
     token = response.token.string
     return token
 
-# ask user to authorize this apikey
-
-def askForAuth(token):
+def authorizeToken(token):
     url = getUrl('http://www.last.fm/api/auth/', {'api_key': apiKey, 'token': token})
     print url
     os.system('firefox "'+ url + '"')
@@ -66,6 +66,14 @@ def getSession(token):
     response = wsRequest(params)
     session = response.session.key.string
     return session
-     
+    
+def getMobileSession(username, password):
+    authToken = md5(username + md5(password))
+    method = 'auth.getMobileSession'
+    params = {'method':method,'username':username, 'authToken':authToken, 'api_key': apiKey}
+    params['api_sig'] = sign(params)
+    response = wsRequest(params)
+    session = response.session.key.string
+    return session
 
 
